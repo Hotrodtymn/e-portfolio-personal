170ddf76f82825fa1bc9a6f6b40051a67df43346
Thomas Babb <tomleebabb@gmail.com>
2026-09-04 19:03:12 -0400

    project update

---
+++ index.js
@@ -0,0 +1,33 @@
+// template_zepbzpv
+// service_37vvo12
+// user_CPDpNHh5-XGqxpOZ6
+
+function contact(event) {
+  event.preventDefault
+  email.js
+  console.log("it worked");  
+  email
+  .sendForm('service_37vvo12', 
+  'template_zepbzpv',
+  event.target,
+   user_CPDpNHh5-XGqxpOZ6
+
+   ).then() => {
+    loading = document.querySelector("model__overlay--loading");
+    success = document.querySelector("model__overlay--success");
+    loading.classList += "model__overlay--visible";
+   }).catch(() => {
+    loading.classList.remove)model overlay--visible")
+    alert( alert(message?: any):void)
+      "The email is temorarily unavailable. Please contact me directly on Tomleebabb@gmail.com"
+   })
+
+
+
+
+
+setTimeout(() => {
+  loading.classList.remove("model__overlay--visible");
+  success.classList += "model__overlay--visible";
+  console.log("this worked 1");
+}, 500);
